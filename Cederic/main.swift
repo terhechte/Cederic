20040fe55469425eba589876ce933a345e980ea5
Benedikt Terhechte <terhechte@gmail.com>
2015-05-21 22:28:56 +0200

    Initial Commit

---
+++ Cederic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cederic
+//
+//  Created by Benedikt Terhechte on 21/05/15.
+//  Copyright (c) 2015 Benedikt Terhechte. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
