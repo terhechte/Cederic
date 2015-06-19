97c1bdb2e2a79425c49d195a8a9b0f6fec3d1376
Benedikt Terhechte <terhechte@gmail.com>
2015-06-19 21:34:25 +0200

    Added a compiler type test to make sure AgentRef can only be called with Objective-C Ref types

--- Cederic/Cederic.swift
+++ Cederic/Cederic.swift
@@ -518,7 +518,7 @@ public func <- <T> (left: Agent<T>, right: T) -> Agent<T> {
     be undone.
 */
 
-final public class AgentRef<T> : _AgentBase<T,(inout T)->Void>  {
+final public class AgentRef<T where T:NSObject> : _AgentBase<T,(inout T)->Void>  {
     
     public init(_ initialState: T) {
         super.init(initialState, validator: nil)
